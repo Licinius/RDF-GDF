184a3dc8b6026104e2887fadb86f6830356532fe
Licinius <florent.dellomo@gmail.com>
2018-11-13 12:54:19 +0100

    Refactor : Suppression des super
    Juste une suppression des super en trop

--- src/POS.java
+++ src/POS.java
@@ -5,14 +5,14 @@ public class POS extends Index {
 
 	@Override
 	public void addElement(int s, int p, int o) {
-		if (super.getIndex().get(p) == null) {
-			super.getIndex().put(p, new HashMap<Integer,HashSet<Integer>>());
-			super.getIndex().get(p).put(o, new HashSet<Integer>());
+		if (getIndex().get(p) == null) {
+			getIndex().put(p, new HashMap<Integer,HashSet<Integer>>());
+			getIndex().get(p).put(o, new HashSet<Integer>());
 		}
-		if (super.getIndex().get(p).get(o) == null) {
-			super.getIndex().get(p).put(o, new HashSet<Integer>());
+		if (getIndex().get(p).get(o) == null) {
+			getIndex().get(p).put(o, new HashSet<Integer>());
 		}
-		super.getIndex().get(p).get(o).add(s);
+		getIndex().get(p).get(o).add(s);
 	}
 
 }
